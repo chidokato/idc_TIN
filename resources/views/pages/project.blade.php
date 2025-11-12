f663781b7096d7bd336c088008905387d6be00f2
chidokato <tuan.pn92@gmail.com>
2025-11-12 14:38:41 +0700

    update

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -63,7 +63,7 @@
             <div class="heading">
                 <div class="left">
                     <h1 class="text-uppercase title-cat">{{$post->name}}</h1>
-                    <div class="address"><i class="icon-location me-1"></i> {{$post->address}}{{ $post->street_id ? ', '.$post->Street->name:'' }}{{$post->ward_id? ', '.$post->Ward->name:''}}{{', '.$post->District->name}}{{', '.$post->Province->name}} <a href="">View on Map</a></div>
+                    <div class="address"><i class="icon-location me-1"></i> {{$post->address}}{{ $post->street_id ? ', '.$post->Street->name:'' }}{{$post->ward_id? ', '.$post->Ward->name:''}}{{', '.$post->District->name}}{{', '.$post->Province->name}} <a target="_blank" href="{{ $post->maps ? $post->maps:null }}">View on Map</a></div>
                 </div>
                 <div class="right">
                     <div class="price"> <span style="font-size: 1.2rem;">Price: </span>
@@ -188,7 +188,6 @@
                             </a>
                         </div>
                     </div>
-
                     @endforeach
                 </div>
             </div>
